70d018c21c81e45e933f42d49091fb1f30d0c3c8
Miguel Alejandro Madrigal Ramírez <143459684+MigaleMR@users.noreply.github.com>
2025-07-03 17:55:01 -0600

    Update index.ts

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,6 +2,13 @@ import type { Request, Response } from "express"
 import User from "../models/User"
 
 export const createAccount = async (req: Request, res: Response) => {
+    const { email } = req.body
+    const userExists = await User.findOne({email})
+    if(userExists){
+        const error = new Error('El Usuario ya está registrado')
+        res.json({error : error.message})
+    }
+    return
     const user = new User(req.body)
     await user.save()
     res.send('Registro creado correctamente')
