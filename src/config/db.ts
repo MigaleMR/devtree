c19422f7e66026faf9a664699af03b834fea7dd6
Miguel Alejandro Madrigal Ramírez <143459684+MigaleMR@users.noreply.github.com>
2025-07-02 18:47:11 -0600

    Coneccion MongoDB

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
+
+
 export const connectDB = async () => {
     try {
-        const url = 'mongodb+srv://alemarra98:Ljndr0812*@devtree.qqnluca.mongodb.net/?retryWrites=true&w=majority&appName=DevTree'
-        const { connection } = await mongoose.connect(url)
-        const url2 = `${connection.host}:${connection.port}`
-        console.log(`MongoDB conectado en ${url2}`)
+        const { connection } = await mongoose.connect(process.env.MONGO_URI)
+        const url = `${connection.host}:${connection.port}`
+        console.log(`MongoDB conectado en ${url}`)
     } catch(error) {
         console.log(error.message);
         process.exit(1);
