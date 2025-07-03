c57616fb8dd19eaf61eefff31674cb3653b3e0fa
Miguel Alejandro Madrigal Ramírez <143459684+MigaleMR@users.noreply.github.com>
2025-07-03 17:36:26 -0600

    colors

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express'
+import { createAccount } from './handlers'
 
 const router = Router()
 
 /** Autenticacion y registro */
-router.get('/auth/register', (req, res) => {
-    console.log(req.body)
-})
+router.get('/auth/register', createAccount)
 
 export default router
