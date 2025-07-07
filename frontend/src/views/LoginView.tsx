2d5467f405359fa6974e09f5ff0be26c846492ee
Miguel Alejandro Madrigal Ramírez <alemarra98@gmail.com>
2025-07-07 16:23:39 -0600

    Layouts

--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom'
 export default function LoginView() {
   return (
     <>
-        <div>RegisterView</div>
         <nav>
             <Link to='/auth/register'>
             No tienes cuenta? Crea una aquí
